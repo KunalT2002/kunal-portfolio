a47ae25c3f72fa4cb941a1a728453fa591be7f1c
kunalT2002 <kunalthorat000@gmail.com>
2026-04-22 17:01:01 +0530

    portfolio ready

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,53 @@
+import { ToastContainer } from "react-toastify";
+import { FaLinkedin, FaWhatsapp, FaPhone, FaEnvelope } from "react-icons/fa";
+
+import "react-toastify/dist/ReactToastify.css";
+import "react";
+
+const Contact = () => {
+  return (
+    <section
+      id="contact"
+      className="flex flex-col items-center justify-center py-24 px-[12vw] md:px-[7vw] lg:px-[20vw]"
+    >
+      {/* Toast Container */}
+      <ToastContainer />
+
+      {/* Section Title */}
+      <div className="text-center mb-16">
+        <h2 className="text-4xl font-bold text-white">CONTACT</h2>
+        <div className="w-32 h-1 bg-purple-500 mx-auto mt-4"></div>
+        <p className="text-gray-400 mt-4 text-lg font-semibold">
+          I’d love to hear from you—reach out for any opportunities or questions!
+        </p>
+      </div>
+
+      {/* Contact Form */}
+      <div className="mt-8 w-full max-w-md bg-[#0d081f] p-6 rounded-lg shadow-lg border border-gray-700">
+        <h3 className="text-xl font-semibold text-white text-center">
+          Connect With Me <span className="ml-1">🚀</span>
+        </h3>
+        <div className="flex flex-wrap justify-center space-x-4 mt-6">
+          {[
+            { icon: <FaLinkedin />, link: "https://www.linkedin.com/in/kunal-thorat-248a54272" },
+            { icon: <FaWhatsapp />, link: "https://wa.me/91702040105" },
+            { icon: <FaPhone />, link: "tel:+91702040105" },
+            { icon: <FaEnvelope />, link: "mailto:kunalthorat581@gmail.com" },
+          ].map((item, index) => (
+              <a
+                  key={index}
+                  href={item.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-xl text-white hover:text-purple-500 transition-transform transform hover:scale-110"
+              >
+                {item.icon}
+              </a>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
---
+++ src/constants.js
@@ -0,0 +1,184 @@
+// Skills Section Logo's
+import androidStudioLogo from './assets/tech_logo/AndroidStudio.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import cssLogo from './assets/tech_logo/css.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import htmlLogo from './assets/tech_logo/html.png';
+import javaLogo from './assets/tech_logo/java.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import ServiceWorkerLogo from './assets/tech_logo/serviceWorkerIcon.png';
+import springbootLogo from './assets/tech_logo/springboot.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import webStromeLogo from './assets/tech_logo/webStrome.png';
+
+// Experience Section Logo's
+import deccanlogicLogo from './assets/company_logo/deccanLogicLogo.jpg';
+import snMasterMindLogo from './assets/company_logo/SN_MasterMind.png';
+
+// Education Section Logo's
+import SPPUlogo from './assets/education_logo/Savitribai_Phule_Pune_University_Logo.png';
+import SRPSlogo from './assets/education_logo/SRPS.jpg';
+
+
+
+export const SkillsInfo = [
+    {
+        title: 'Frontend',
+        skills: [
+            {name: 'HTML', logo: htmlLogo},
+            {name: 'CSS', logo: cssLogo},
+            {name: 'Java Script', logo: javascriptLogo},
+            {name: 'React JS', logo: reactjsLogo},
+            {name: 'React Native', logo: reactjsLogo},
+            {name: 'Redux', logo: reduxLogo},
+            {name: 'Material UI', logo: materialuiLogo},
+            {name: 'Bootstrap', logo: bootstrapLogo},
+            {name: 'Service Worker', logo: ServiceWorkerLogo},
+        ],
+    },
+    {
+        title: 'Backend',
+        skills: [
+            {name: 'Spring boot', logo: springbootLogo},
+            {name: 'Node JS', logo: nodejsLogo},
+            {name: 'Express JS', logo: expressjsLogo},
+            {name: 'MySQL', logo: mysqlLogo},
+            {name: 'Postgre SQL', logo: postgreLogo},
+        ],
+    },
+    {
+        title: 'Languages',
+        skills: [
+            {name: 'Java', logo: javaLogo},
+            {name: 'Python', logo: pythonLogo},
+            {name: 'Java Script', logo: javascriptLogo},
+            {name: 'Type Script', logo: typescriptLogo},
+        ],
+    },
+    {
+        title: 'Tools',
+        skills: [
+            {name: 'Git', logo: gitLogo},
+            {name: 'GitHub', logo: githubLogo},
+            {name: 'VS Code', logo: vscodeLogo},
+            {name: 'Postman', logo: postmanLogo},
+            {name: 'Web Strome', logo: webStromeLogo},
+            {name: 'Android Studio', logo: androidStudioLogo},
+        ],
+    },
+];
+
+export const experiences = [
+    {
+        id: 0,
+        img: deccanlogicLogo,
+        role: "Fullstack Developer",
+        company: "DeccanLogic Private Limited",
+        date: "October 2024 - March 2026",
+        desc: " Developed and maintained full-stack applications using React and Node.js with PostgreSQL. Designed and optimized RESTful APIs for secure and scalable data exchange. Implemented real-time features using WebSockets and Socket.io for interactive system communication. Built responsive UI components using React and Bootstrap. Improved database query performance and managed schema updates using pgAdmin. Collaborated in Agile teams to deliver production-ready features on schedule. Led issue resolution and coordinated with cross-functional teams for timely bug fixes.",
+        skills: [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "React JS",
+            "TypeScript",
+            "Node JS",
+            "Boostrap",
+            "PostgressSQL",
+            "Redux",
+            "Service Worker",
+        ],
+    },
+    {
+        id: 1,
+        img: snMasterMindLogo,
+        role: "Fullstack Engineer",
+        company: "SN MasterMind Private Limited",
+        date: "April 2023 - March 2024",
+        desc: "Contributed to a banking software platform for Path-Sanstha financial institutions, handling deposits, loans, accounts and daily collections. Developed full-stack features using React (frontend) and Spring Boot / Java (backend), with MySQL database integration. Built a React Native mobile application for field agents to perform and track daily collections for Path-Sansthas.  Designed and tested RESTful APIs using Postman and managed source code with Git. ",
+        skills: [
+            "React Native",
+            "Redux",
+            "Java",
+            "SpringBoot",
+            "CSS",
+            "SQL",
+        ],
+    },
+];
+
+export const education = [
+    {
+        id: 0,
+        img: SPPUlogo,
+        school: "Savitribai Phule Pune University",
+        date: "July 2020 - June 2024",
+        grade: "8.14 CGPA",
+        desc: "Here’s your updated version with the correct university: I have completed my Bachelor of Engineering (BE) in Computer Engineering from Savitribai Phule Pune University. During my academic journey, I developed a strong foundation in core areas of computer science, including programming, data structures, algorithms, and software development. I have studied key subjects such as Data Structures, Object-Oriented Programming, Database Management Systems, Operating Systems, Computer Networks, and Web Technologies, which helped me build both theoretical understanding and practical skills.",
+        degree: "Bachelor of Engineering - Computer Engineering",
+    },
+    {
+        id: 1,
+        img: SRPSlogo,
+        school: "Sharad Rural Public School, RaghunathNagar",
+        date: "June 2019 - March 2020",
+        grade: "65%",
+        desc: "I completed my Class 12 education from Sharad Rural Public School, Raghunathnagar, under the Maharashtra State Board, where I studied Physics, Chemistry, and Mathematics (PCM) with Computer Science.",
+        degree: "HSC - PCM with Computer Science",
+    },
+    {
+        id: 2,
+        img: SRPSlogo,
+        school: "Sharad Rural Public School, RaghunathNagar",
+        date: "June 2017 - March 2018",
+        grade: "82.60%",
+        desc: "I completed my Class 10 education from Sharad Rural Public School, Raghunathnagar, under the Maharashtra State Board",
+        degree: "SSC",
+    },
+];
+
+export const projects = [
+    {
+        id: 0,
+        title: "Simplicity-Fintech",
+        description:
+            "Engineered a full-stack fintech platform supporting SIP, SWP, STP, Switch, Redemption, and Lumpsum investments with NSE integration. Developed secure REST APIs handling 50+ daily financial transactions. Built a React Native mobile app for portfolio tracking and transactions. Implemented real-time chat using WebSockets and Socket.io, along with browser push notifications via Service Workers. Secured workflows using JWT authentication and RBAC, and optimized PostgreSQL schemas for high-volume financial data.",
+        // image: fintechLogo,
+        tags: ["React JS", "React Native", "Node.js", "PostgreSQL", "WebSockets", "Socket.io"]
+    },
+    {
+        id: 1,
+        title: "Evolvus CM-App",
+        description:
+            "Developed a dynamic customer management application with React and Node.js, enabling automatic frontend updates based on backend data changes. Implemented a multi-step QA workflow for bio-sample validation. Built RESTful APIs for data creation, updates, and QA processes. Managed PostgreSQL database for efficient storage and seamless synchronization.",
+        // image: evolvusLogo,
+        tags: ["React JS", "Node.js", "PostgreSQL", "REST API"]
+    },
+    {
+        id: 2,
+        title: "PathWin – Banking Software",
+        description:
+            "Contributed to a banking system managing deposits, loans, accounts, and collections. Developed secure backend modules using Spring Boot with RBAC-based access control. Built REST APIs ensuring transaction integrity and seamless communication between frontend and backend systems.",
+        // image: pathwinLogo,
+        tags: ["React JS", "Spring Boot", "Java", "REST API", "RBAC"]
+    },
+    {
+        id: 3,
+        title: "Algo Trading App",
+        description:
+            "Developed a full-stack stock analysis and trading platform using Flask and React. Integrated Yahoo Finance API to fetch real-time and historical data for NIFTY 50 stocks. Implemented technical analysis strategies to identify 5–10% profit opportunities within one month and built algorithms to recommend top 3–5 stocks weekly. Designed interactive dashboards for trend and price analysis. Integrated Angel One Smart API for real-time trade execution and optimized data pipelines for multi-stock analysis.",
+        // image: algoLogo,
+        tags: ["React JS", "Python", "Flask", "Yahoo Finance API", "Angel One API"]
+    },
+];
